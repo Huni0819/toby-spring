d3cc9e9e44d7d395b01d0fbf8bb6ef9159e809c7
Huni0819 <orutaku15@gmail.com>
2025-05-24 20:02:03 +0900

    [refactor] 스프링 컨테이너와 의존관계 주입

--- hellospring/src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ hellospring/src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,13 +1,20 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+
+    @Bean
     public PaymentService paymentService() {
 
         return new PaymentService(this.exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
 
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }
