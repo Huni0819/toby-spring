2f47ec7df6480005ba12a37044432addd2b27ade
Huni0819 <orutaku15@gmail.com>
2025-06-02 11:14:46 +0900

    JUnit 테스트 작성

---
+++ hellospring/src/main/java/tobyspring/hellospring/Sort.java
@@ -0,0 +1,14 @@
+package tobyspring.hellospring;
+
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Sort {
+
+    public List<String> sortByLength(List<String> list) {
+
+        list.sort(Comparator.comparingInt(o -> o.length()));
+        return list;
+    }
+}
---
+++ hellospring/src/test/java/tobyspring/hellospring/SortTest.java
@@ -0,0 +1,46 @@
+package tobyspring.hellospring;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class SortTest {
+
+    Sort sort;
+
+    @BeforeEach
+    void setup() {
+
+        sort = new Sort();
+    }
+
+    @Test
+    void sort() {
+
+        List<String> lists = sort.sortByLength(Arrays.asList("aa", "b"));
+
+        Assertions.assertThat(lists)
+                .isEqualTo(List.of("b", "aa"));
+    }
+
+    @Test
+    void sort3Items() {
+
+        List<String> lists = sort.sortByLength(Arrays.asList("aa", "ccc", "b"));
+
+        Assertions.assertThat(lists)
+                .isEqualTo(List.of("b", "aa", "ccc"));
+    }
+
+    @Test
+    void sortAlreadySorted() {
+
+        List<String> lists = sort.sortByLength(Arrays.asList("b", "aa", "ccc"));
+
+        Assertions.assertThat(lists)
+                .isEqualTo(List.of("b", "aa", "ccc"));
+    }
+}
